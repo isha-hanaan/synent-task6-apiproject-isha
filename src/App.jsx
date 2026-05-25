4152f85c7fd2dcb041793463007815d7d3c71e03
isha-hanaan <hisha981@gmail.com>
2026-05-25 09:46:58 +0530

    Integrated OpenWeather API key

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,37 @@ import { useState } from "react";
 
 function App() {
     const [city, setCity] = useState("");
+    const API_KEY = import.meta.env.VITE_API_KEY;
+
+    const [weather, setWeather] = useState(null);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState("");
+
+    const fetchWeather = async () => {
+        if (!city.trim()) return;
+        setLoading(true);
+        setError("");
+        setWeather(null);
+
+        try {
+            const response = await fetch(
+                `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
+            );
+
+            if (!response.ok) {
+                throw new Error("City not found");
+            }
+
+            const data = await response.json();
+
+            setWeather(data);
+
+        } catch (err) {
+            setError(err.message);
+        } finally {
+            setLoading(false);
+        }
+    };
 
     return (
         <div className="app">
@@ -17,7 +48,20 @@ function App() {
                     onChange={(e) => setCity(e.target.value)}
                 />
 
-                <button>Search</button>
+                <button onClick={fetchWeather}>
+                    Search
+                </button>
+
+                {loading && <p>Loading weather...</p>}
+                {error && <p>{error}</p>}
+
+                {weather && (
+                    <div>
+                        <h2>{weather.name}</h2>
+                        <p>{weather.main.temp}°C</p>
+                        <p>Humidity: {weather.main.humidity}%</p>
+                    </div>
+                )}
 
             </div>
         </div>
